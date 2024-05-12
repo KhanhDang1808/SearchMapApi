be9a46a1b52954d34d2ada3dbea017c844435ace
KhanhDang1808 <khanhdang2002pt@gmail.com>
2024-05-12 17:12:27 +0700

    Add Note: Get list Address

--- app/src/main/java/com/example/getgooglemapapi/MainActivity.kt
+++ app/src/main/java/com/example/getgooglemapapi/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.getgooglemapapi
 
 import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.common.util.CollectionUtils.listOf
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
@@ -27,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         }
         // Create a new Places client instance.
         val placesClient = Places.createClient(this)
-
         val autocompleteFragment =
             supportFragmentManager.findFragmentById(R.id.fragment) as AutocompleteSupportFragment?
         autocompleteFragment!!.setPlaceFields(
@@ -53,21 +56,48 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         })
+
+
+//                                val placesClient2 = Places.createClient(this)
+//
+//                        // Get the text from the AutocompleteSupportFragment
+//                                val query = "ha noi"
+//
+//                        // Create a FindAutocompletePredictionsRequest object
+//                                val request = FindAutocompletePredictionsRequest.builder()
+//                                    .setQuery(query)
+//                                    .build()
+//
+//                        // Use PlacesClient to find autocomplete predictions
+//                                placesClient2.findAutocompletePredictions(request)
+//                                    .addOnSuccessListener { response: FindAutocompletePredictionsResponse ->
+//                                        for (prediction in response.autocompletePredictions) {
+//                                            Log.i("khanh", "Place prediction: ${prediction.getFullText(null)}")
+//                                        }
+//                                    }
+//                                    .addOnFailureListener { exception: Exception ->
+//                                        if (exception is ApiException) {
+//                                            Log.e("khanh", "Place not found: ${exception.statusCode}")
+//                                        } else {
+//                                            Log.e("khanh", "Unexpected error: ${exception.localizedMessage}")
+//                                        }
+//                                    }
     }
 
-    private fun getDirection(to:String){
-        try{
+    private fun getDirection(to: String) {
+        try {
 //            val uri: Uri =Uri.parse("https://www.google.com/maps/dir/$from/$to")
 //            val intent = Intent(Intent.ACTION_VIEW,uri)
             val uri = "google.navigation:q=$to"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
             intent.setPackage("com.google.android.apps.maps")
-            intent.flags= (Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
-        }catch (exception:ActivityNotFoundException){
-            val uri: Uri =Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps")
-            val intent = Intent(Intent.ACTION_VIEW,uri)
-            intent.flags= (Intent.FLAG_ACTIVITY_NEW_TASK)
+        } catch (exception: ActivityNotFoundException) {
+            val uri: Uri =
+                Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
     }
